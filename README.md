d57ca973844fc4211d3799a70643f485af9b1a74
ysnbogt <ysnbogt@gmail.com>
2023-05-21 17:54:59 +0900

    label, due date, and percentage functions added

--- README.md
+++ README.md
@@ -1,9 +1,55 @@
 <h1 align="center">Toggle Markdown Checkbox</h1>
 
+## Installation
+
 ```zsh
 $ npm install --global toggle-markdown-checkbox
 ```
 
+## Usage
+
 ```zsh
-$ check <file-name>
+$ check <file-path>
+$ check rate <file-path>
+```
+
+## Example
+
+**`task.md`**
+
+```md
+---
+GET: green
+POST: cyan
+PUT: yellow
+DELETE: red
+---
+
+- [x] GET: /api/task/1 (due: 2023-04-01)
+- [x] POST: /api/task (due: 2023-05-30)
+- [x] PUT: /api/task/3 (due: 2023-05-28)
+- [x] DELETE: /api/task/4 (due: 2023-06-05)
+- [ ] task5 (due: 2023-05-25)
+- [ ] task6
+```
+
+```zsh
+$ check ./task.md
+No overdue tasks.
+? Select items to toggle …
+✔ GET /api/task/1
+✔ POST /api/task
+✔ PUT /api/task/3
+✔ DELETE /api/task/4
+✔ task5
+✔ task6
+```
+
+```zsh
+$ check rate ./task.md
+No overdue tasks.
+
+Checkbox State Report: 4/6 checkboxes checked.
+
+Completion Rate: 66.67%
 ```
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,23 @@
 import { prompt } from 'enquirer';
 import { resolve } from 'path';
 import { readFileSync, writeFileSync, existsSync } from 'fs';
+import frontMatter from 'front-matter';
+import chalk, { type ColorName } from 'chalk';
 
 type CheckBox = {
   index: number;
   checked: boolean;
   label: string;
+  extraLabel: string;
+  due: Date | undefined;
+};
+
+type Colors = Record<string, ColorName>;
+
+type FileContent = {
+  content: string;
+  checkboxes: CheckBox[];
+  attributes: Colors;
 };
 
 type PromptOptions = {
@@ -36,14 +48,38 @@ function updateCheckboxesState(
   });
 }
 
+function reportCheckboxState(checkboxes: CheckBox[]): void {
+  const totalCheckboxes = checkboxes.length;
+  const checkedCheckboxes = checkboxes.filter(cb => cb.checked).length;
+  const completionRateStr = (
+    (checkedCheckboxes / totalCheckboxes) *
+    100
+  ).toFixed(2);
+  const completionRate = parseFloat(completionRateStr);
+
+  console.log(
+    `\nCheckbox State Report: ${checkedCheckboxes}/${totalCheckboxes} checkboxes checked.\n`
+  );
+
+  if (completionRate < 50) {
+    console.log(`\x1b[31mCompletion Rate: ${completionRate}%\x1b[0m\n`);
+  } else if (completionRate < 75) {
+    console.log(`\x1b[33mCompletion Rate: ${completionRate}%\x1b[0m\n`);
+  } else {
+    console.log(`\x1b[32mCompletion Rate: ${completionRate}%\x1b[0m\n`);
+  }
+}
+
 function updateFileContent(
   fileName: string,
   fileContent: string,
   checkboxes: CheckBox[]
 ): void {
   let updatedContent = fileContent;
-  checkboxes.forEach(({ checked, label, index }) => {
-    const newCheckbox = `- [${checked ? 'x' : ' '}]${label}`;
+  checkboxes.forEach(({ checked, label, extraLabel, index }) => {
+    const newCheckbox = `- [${checked ? 'x' : ' '}] ${
+      extraLabel && extraLabel + ':'
+    }${label}`;
     updatedContent =
       updatedContent.substring(0, index) +
       newCheckbox +
@@ -54,24 +90,42 @@ function updateFileContent(
   console.log('File updated successfully.');
 }
 
-async function main() {
-  const fileName = resolve(process.argv[2]);
+function reportOverdueTasks(checkboxes: CheckBox[]): void {
+  const overdueTasks = checkboxes.filter(
+    cb => !cb.checked && cb.due && cb.due < new Date()
+  );
 
+  if (overdueTasks.length > 0) {
+    console.log('\x1b[31mOverdue tasks:\x1b[0m');
+    overdueTasks.forEach(task =>
+      console.log(
+        `- ${task.label.trim()} (due: ${task.due!.toISOString().split('T')[0]})`
+      )
+    );
+  } else {
+    console.log('\x1b[32mNo overdue tasks.\x1b[0m');
+  }
+}
+
+function getFileContent(fileName: string): FileContent {
   if (!existsSync(fileName)) {
     console.error(`\x1b[31mError:\x1b[0m File '${fileName}' does not exist.`);
     process.exit(1);
   }
 
   const fileContent = readFileSync(fileName, 'utf-8');
-  const regex = /^- \[(x| )\](.*)$/gm;
-  let match;
+  const regex = /- \[(x| )\] (([ -a-zA-Z0-9]+):)?(.*?)( \(due: ([^)]+)\))?$/gm;
   const checkboxes: CheckBox[] = [];
+  const attributes = frontMatter(fileContent).attributes;
 
+  let match;
   while ((match = regex.exec(fileContent)) !== null) {
     checkboxes.push({
       index: match.index,
       checked: match[1] === 'x',
-      label: match[2],
+      label: match[4],
+      extraLabel: match[3] || '',
+      due: match[6] ? new Date(match[6]) : undefined,
     });
   }
 
@@ -81,6 +135,25 @@ async function main() {
     );
     process.exit(1);
   }
+
+  return {
+    content: fileContent,
+    checkboxes,
+    attributes: attributes as Colors,
+  };
+}
+
+async function rateCommand(fileName: string) {
+  const { checkboxes } = getFileContent(fileName);
+
+  reportOverdueTasks(checkboxes);
+  reportCheckboxState(checkboxes);
+}
+
+async function defaultCommand(fileName: string) {
+  const { content, checkboxes, attributes } = getFileContent(fileName);
+
+  reportOverdueTasks(checkboxes);
 
   const initial = checkboxes
     .map((checkbox, index) => checkbox.checked && index)
@@ -93,7 +166,14 @@ async function main() {
     initial,
     choices: checkboxes.map((checkbox, index) => ({
       name: index.toString(),
-      message: checkbox.label,
+      message:
+        `${
+          checkbox.extraLabel
+            ? chalk[attributes[checkbox.extraLabel] || 'gray'](
+                checkbox.extraLabel.trim()
+              )
+            : ''
+        }` + checkbox.label,
     })),
   } as PromptOptions)
     .then((response: unknown) => {
@@ -102,11 +182,25 @@ async function main() {
       ).checkboxes.map((selected: string) => Number(selected));
 
       updateCheckboxesState(checkboxes, selectedIndices);
-      updateFileContent(fileName, fileContent, checkboxes);
+      updateFileContent(fileName, content, checkboxes);
     })
     .catch((error: Error) => {
       console.error('Error:', error);
     });
+}
+
+async function main() {
+  const subCommand = process.argv[2];
+
+  switch (subCommand) {
+    case 'rate':
+      // eslint-disable-next-line no-case-declarations
+      const fileName = resolve(process.argv[3]);
+      await rateCommand(fileName);
+      break;
+    default:
+      defaultCommand(subCommand);
+  }
 }
 
 main();
